463a82792166eb0b41366e7283bae497581a07e1
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-16 02:12:44 +0330

    css modified + JS ready
    final

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const buttons = document.querySelectorAll('button');
+const toggler = (button) => {
+  button.parentNode.classList.toggle('active');
+};
 
+buttons.forEach((button) => {
+  button.addEventListener('click', toggler.bind(null, button));
+});
